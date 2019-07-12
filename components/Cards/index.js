6bd653e87345587e4059a0b33dde7241167b260c
gisellehargrove <gisellehargrove@gmail.com>
2019-07-12 13:07:05 -0400

    create a new card for each article

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,14 +18,14 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
-
 const cardCreator = (options) => {
+  const cardsContainer = document.querySelector('.cards-container');
   const card = document.createElement('div');
   card.classList.add('card');
 
   const headline = document.createElement('div');
   headline.classList.add('headline');
-  headline.textContent = '';
+  headline.textContent = options.headline;
   card.appendChild(headline);
 
   const authorContainer = document.createElement('div');
@@ -35,13 +35,28 @@ const cardCreator = (options) => {
   imgContainer.classList.add('img-container');
 
   const imgElem = document.createElement('img');
-  imgElem.src = '';
+  imgElem.src = options.authorPhoto;
 
   const authorName = document.createElement('span');
-  authorName.textContent = 'By ' + '';
+  authorName.textContent = 'By ' + options.authorName;
 
   imgContainer.appendChild(imgElem);
   authorContainer.appendChild(imgContainer);
   authorContainer.appendChild(authorName);
   card.appendChild(authorContainer);
+  cardsContainer.appendChild(card);
+
+  return card;
 };
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(response => {
+  const articles = response.data.articles;
+  const categories = Object.keys(response.data.articles);
+
+  categories.forEach(category => {
+    articles[category].forEach(article => {
+      cardCreator(article);
+    });
+  });
+
+});
